b5ff9032a3ab8c9de6603a14a56ec80875085d03
AmirTGitHub <amir.gevari@paconsulting.com>
2020-01-13 21:18:15 +0000

    add a validation for the array

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 exports.requestMultipleUrls = async (urls)=> {
-  const fetch = require ('node-fetch')
+  const fetch = require("node-fetch")
+  if (!Array.isArray(urls)) {
+    throw {
+      error: "Array is not provided",
+      message: "Please pass an array of urls to the function"
+    };
+  }
+  
   const result = await Promise.all(
     urls.map(async (url, index) => {
       try {
